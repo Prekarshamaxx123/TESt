94e7ee34acd5b2d6c794112826fdd53b8a4b0925
DarkCyberPrekash <92394796+Prekarshamaxx123@users.noreply.github.com>
2022-05-16 15:41:53 +0530

    Update Pro.py

--- Pro.py
+++ Pro.py
@@ -251,15 +251,7 @@ if len(sys.argv) == 2:
 		Main()
 
 try:Main()
-except Exception as e:exit 
-	stre(W = '\033[97;1m' 
-R = '\033[91;1m' 
-G = '\033[92;1m' 
-Y = '\033[93;1m' 
-B = '\033[94;1m'
-P = '\033[95;1m'
-C = '\033[96;1m'
-N = '\x1b[0m'
+
 
 
 
