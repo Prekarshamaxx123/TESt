f6ca60525884a24117d7b18440b2fc79b82c4839
DarkCyberPrekash <92394796+Prekarshamaxx123@users.noreply.github.com>
2022-05-25 05:12:49 +0530

    Update 10.py

--- 10.py
+++ 10.py
@@ -76,7 +76,8 @@ class Main:
 			os.remove("data/token");os.remove("data/coki")
 			exit(" ! Your token Expired ! ")
 		print(
-			f" >> Your Account: {info['name']}, {info['id']} << "
+			f" >> Your Account: {info['name']}, {info['id']} << " 
+os.system('xdg-open https://www.facebook.com/PREKASH.I.AM.HACKER.Im.silent')
 		)
 		print(
 			"""
